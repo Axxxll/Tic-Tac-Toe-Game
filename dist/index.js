64cb8959ca0b3212d1b903512a41a47f84e7eb0a
Axxxll <alex.plam.atan@gmail.com>
2023-01-30 02:00:10 +0200

    Implementing the logic

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,58 @@
 "use strict";
-console.log("Hello World!");
+let circleTurn = false;
+let gameIsRuning = false;
+const X_CLASS = "x";
+const CIRCLE_CLASS = "circle";
+const cells = document.querySelectorAll("[data-cell]");
+const board = document.getElementById("board");
+const winingCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+startGame();
+function startGame() {
+    cells.forEach(cell => {
+        cell.addEventListener("click", () => { addMarker(cell); }, { once: true });
+    });
+    addHoverEffect();
+}
+function renderGame() {
+    if (checkForWin()) {
+        console.log("someone has won!");
+    }
+    else {
+        switchTurn();
+        addHoverEffect();
+    }
+}
+function checkForWin() {
+    return winingCombinations.some(element => {
+        return element.every(index => {
+            return cells[index].classList.contains(X_CLASS || CIRCLE_CLASS);
+        });
+    });
+}
+function addMarker(cell) {
+    const currentTurn = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    cell.classList.add(currentTurn);
+    renderGame();
+}
+function switchTurn() {
+    circleTurn = !circleTurn;
+}
+function addHoverEffect() {
+    board === null || board === void 0 ? void 0 : board.classList.remove(X_CLASS);
+    board === null || board === void 0 ? void 0 : board.classList.remove(CIRCLE_CLASS);
+    if (circleTurn) {
+        board === null || board === void 0 ? void 0 : board.classList.add(CIRCLE_CLASS);
+    }
+    else {
+        board === null || board === void 0 ? void 0 : board.classList.add(X_CLASS);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,68 @@
-console.log("Hello World!")
+let circleTurn: boolean = false
+let gameIsRuning: boolean = false
+const X_CLASS: string = "x"
+const CIRCLE_CLASS: string = "circle"
+
+const cells = document.querySelectorAll("[data-cell]")
+const board = document.getElementById("board")
+const winingCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+startGame()
+
+function startGame(): void {
+    cells.forEach(cell => {
+        cell.addEventListener("click", () => { addMarker(cell) }, { once: true })
+    })
+    addHoverEffect()
+}
+
+function renderGame(): void {
+    if (checkForWin()) {
+        console.log("someone has won!")
+    }
+    else {
+        switchTurn()
+        addHoverEffect()
+    }
+    // See if the game has ended. Either a draw or a win
+    // See who's turn it is
+    // Add a mark to the selected cell
+    // Switch turns
+}
+
+function checkForWin(): boolean {
+    return winingCombinations.some(element => {
+        return element.every(index => {
+           return cells[index].classList.contains(X_CLASS || CIRCLE_CLASS)
+        })
+    })
+}
+
+function addMarker(cell: Element): void {
+    const currentTurn = circleTurn ? CIRCLE_CLASS : X_CLASS
+    cell.classList.add(currentTurn)
+    renderGame()
+}
+
+function switchTurn(): void {
+    circleTurn = !circleTurn
+}
+
+function addHoverEffect(): void {
+    board?.classList.remove(X_CLASS)
+    board?.classList.remove(CIRCLE_CLASS)
+    if (circleTurn) {
+        board?.classList.add(CIRCLE_CLASS)
+    } else {
+        board?.classList.add(X_CLASS)
+    }
+}
